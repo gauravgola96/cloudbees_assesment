efa14d996d2ac3141ce07528ea3f0730f5e6655d
Gaurav Gola <gaurav.gola@conversenow.ai>
2025-05-26 19:50:24 +0530

    Added server cmd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/gauravgola96/cloudbees_assesment/pkg/server"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	srv, err := server.NewHttpServer()
+	if err != nil {
+		log.Fatalf("error starting server: %s", err.Error())
+		return
+	}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	sig := <-quit
+	log.Printf("server shutdown due to %s", sig.String())
+	err = srv.Shutdown(context.TODO())
+	if err != nil {
+		log.Fatalf("error server gracefull %s", err.Error())
+		return
+	}
+	log.Printf("server stopped gracefully")
+}
